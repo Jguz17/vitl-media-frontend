9b7d316360c5bce17cdf42e67e47f41042ec6cc1
jguz17 <johnathanguzman17@gmail.com>
2020-09-26 10:55:49 -0500

    able to get value from input and checkbox fields.

--- js/main/landing-page.js
+++ js/main/landing-page.js
@@ -1,4 +1,28 @@
 document.querySelector('.contact-form').addEventListener('submit', (e) => {
     e.preventDefault();
-    location.replace('../../pages/thank-you-1.html')
+    
+    const name = document.querySelector('#form-name').value
+    const phone = document.querySelector('#form-phone').value
+    const email = document.querySelector('#form-email').value
+
+    let phoneCheckbox;
+    let emailCheckbox;
+
+    if (document.getElementById('phone').checked) {
+        phoneCheckbox = true;
+    } else {
+        phoneCheckbox = false
+    }
+
+    if (document.getElementById('email').checked) {
+        emailCheckbox = true
+    } else {
+        emailCheckbox = false
+    }
+
+    const formObj = { name, phone, email, phoneCheckbox, emailCheckbox }
+
+    console.log(formObj)
+
+    // location.replace('../../pages/thank-you-1.html')
 })
